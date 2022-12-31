86eaef4eaac6a4b09f7e17472864c715fe6949f9
nick-ras <nick-ras@mail.dk>
2022-12-31 22:49:28 +0100

    version of ex01

--- ex01/src/phonebook.cpp
+++ ex01/src/phonebook.cpp
@@ -7,46 +7,40 @@ using namespace std;
 class	Contact
 {
 	public:
-		std::string str[5];
+		std::string str[5] = {"", "", "", "", ""};
 		//first_name; last_name;nickname;phone_number; darkest_secret;
-		std::string questions[5] = {"Enter a first name: ", "Enter a last name: ", "Enter a nickname: " , "Enter a phone number: ", "Enter a darkest: "};
-		
-		void return_contact() 
-		{
-			int i = 0;
-			while (i < 5)
-			{
-				std::cout << str[i] << std::endl;
-				i++;
-			}
-		};
 };
 
 class	PhoneBook
 {
 	public:
-		Contact *contacts[8];
-		std::string get[5] = {"first name", "last name", "nickname", "phone number", "darkest secret"};
+		Contact *contacts[8] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+		std::string questions[5] = {"Enter a first name: ", "Enter a last name: ", "Enter a nickname: " , "Enter a phone number: ", "Enter a darkest: "};
+		~PhoneBook()
+		{			
+		int i = 0;
 
-
+		std::cout << "DESTRUCTOR" << std::endl;			 
+			while (contacts[i])
+			{
+				delete contacts[i];
+				cout << "DELETED CONTACT" << endl;
+				i++;
+			}
+		}
 		void add_contact(int contact_id)
 		{
 			int j = 0;
 
+			contacts[contact_id] = new Contact();
 			while(j < 5)
 			{
-				std::cout << "IN ADD" << std::endl;
-				contacts[contact_id] = new Contact();
-				std::cout << contacts[contact_id]->questions[j] << std::endl; //create contact?
-				if (!getline(cin, contacts[contact_id]->str[j]))
-					std::cout << "ERROR" << std::endl;
+				std::cout << questions[j] << std::endl;
+				std::cin >> contacts[contact_id]->str[j];
+				// if (!getline(cin, contacts[contact_id]->str[j])) //doesnt get first name
+					// std::cout << "ERROR" << std::endl;
 				j++;
 			}
-		};
-
-		Contact get_contact(int i)
-		{
-			return (*contacts[i]);
 		}
 
 		int compare(string s1, string s2) 
@@ -54,7 +48,7 @@ class	PhoneBook
 			int i = 0;
 			while (s1[i] != '\0' || s2[i] != '\0')
 			{
-				printf("s1: %c, s2: %c\n", s1[i], s2[i]);
+				//printf("s1: %c, s2: %c\n", s1[i], s2[i]);
 				if ((unsigned char)s1[i] < (unsigned char)s2[i])
 				{
 					return ((unsigned char)s1[i] - (unsigned char)s2[i]);
@@ -68,26 +62,45 @@ class	PhoneBook
 			return (0);
 		}
 
-		// int	search(string cnt, string word, Contact *obj) 
-		// {
-		// 		if (compare(cnt, word) == 0)
-		// 		{
-		// 			 //DIESPLAY 3 columns see subject pdf
-		// 			std::cout.write(obj->str[0].c_str(), 10);
-		// 			std::cout << " |" << std::endl;
-		// 			return (0);
-		// 		}
-		// 	return (1);
-		// }
+		int	search(string word) 
+		{
+			int i = 0;
+			int j = 0;
+
+			std::cout << "IN SEARCH" << std::endl;
+			while (contacts[i])
+			{
+				while(&contacts[i]->str[j])
+				{
+					if (compare(contacts[i]->str[j], word) == 0)
+					{
+						j = 0;
+						while (j < 5)
+						{
+							if (j < 4)
+							std::cout << contacts[i]->str[j] << " |" << std::endl;
+							else if (j < 3)
+								std::cout << contacts[i]->str[j] << std::endl;
+							j++;
+						}
+						return (1);
+					}
+					j++;
+				}
+				i++;
+			}
+			return (0);
+		}
+
+		
 };
 
-
+//first free and the automatic destroy
 int main(int argc, char **argv)
 {
 	if (argc == 1 && argv[0])
 	{
 		PhoneBook book = PhoneBook();
-		std::cout << "Enter a search term: " << std::endl;
 		std::string input;
 
 		int add = 0;
@@ -97,30 +110,26 @@ int main(int argc, char **argv)
 				std::cin >> input;
 				if(!(book.compare(input, "ADD")))
 				{
-					// std::cout << "equal in add" << std::endl;
 					book.add_contact(add);
-					add++;
-					//DELETE instance?
+					add++; //delete here
 				}
-			// 	else if(input == "SEARCH")
-			// 	{
-			// 		string word;
+				else if(!(book.compare(input, "SEARCH")))
+				{
+					string word;
 
-			// 		std::cout << "Enter a search term: " << std::endl;
-			// 		std::cin >> word;
-			// 		int j = 0;
-			// 		std::cout << book.get_contact(j).str[0] << std::endl;
-			// 		// while (book.get_contact(j) == NULL)
-			// 		// {
-			// 		// 	// while(contacts[j]->str[i])
-			// 		// 	// {
-			// 		// 	// 	if (search((contacts[j]).str[i], word, contacts[j]) == 0)
-			// 		// 	// 		return ;
-			// 		// 	// 	i++;
-			// 		// 	// }
-			// 		// } 
-			// 	}
+					std::cout << "Enter a search term: " << std::endl;
+					std::cin >> word; //if not getline both
+					if (!book.search(word))
+						std::cout << "FOUND NOTHING" << std::endl;
+				}
+				else if(!(book.compare(input, "EXIT")))
+				{
+					std::cout << "EXITING" << std::endl;
+					return (0);
+				}
+				else
+					std::cout << "INVALID COMMAND" << std::endl;
 			}
 	}
 	return (0);
-}
+};
